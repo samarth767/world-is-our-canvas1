bbc7262f9c3d42960749fe4f5fb1bf66062c2b79
samarth <shashidhar.sanjeeva@samsacademy.co.in>
2020-06-21 19:43:01 +0530

    done

---
+++ sketch.js
@@ -0,0 +1,44 @@
+var database;
+var canvas;
+
+function setup() {
+    canvas = createCanvas(1200,00);
+    database = firebase.database();
+}
+
+var drawing = [];
+var db_drawing  = [];
+
+
+function draw() {
+    background(255);
+    readPosition();
+    beginShape();
+    stroke(0);
+    strokeWeight(4);
+    noFill();
+    if(db_drawing!=null){
+        for(var i = 0 ; i<db_drawing.length ; i++){
+            vertex(db_drawing[i].x,db_drawing[i].y);
+            endShape();
+        }
+    }
+}
+
+function readPosition() {
+  database.ref('drawing/d').on('value',function (data){
+      db_drawing = data.val();
+  })
+}
+
+function mouseDragged(){
+    var point = {
+        x:mouseX,
+        y:mouseY
+    }
+    drawing.push(point);
+    var databaseref = database.ref('drawing');
+    databaseref.set({
+        "d":drawing
+    })
+}
